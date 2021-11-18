86ac1e7bad70c7d1b815bcce976e774823110a27
Teredic <40125338+Teredic@users.noreply.github.com>
2021-11-18 22:44:04 +0100

    fix compilation error

--- src/crypto/rx/Rx.cpp
+++ src/crypto/rx/Rx.cpp
@@ -98,7 +98,7 @@ bool xmrig::Rx::init(const T &seed, const RxConfig &config, const CpuConfig &cpu
 
     randomx_set_scratchpad_prefetch_mode(config.scratchpadPrefetchMode());
     randomx_set_huge_pages_jit(cpu.isHugePagesJit());
-    randomx_set_optimized_dataset_init(algo != Algorithm::RX_XLA ? config.initDatasetAVX2() : 0);
+    randomx_set_optimized_dataset_init(config.initDatasetAVX2() : 0);
 
 #   ifdef XMRIG_FEATURE_MSR
     if (!RxMsr::isInitialized()) {
